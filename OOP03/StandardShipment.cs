cf3e0f619917401b9ea2d24d60c58bfe8db0f9ae
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-11 04:14:46 +0300

    standardshipment

--- OOP03/StandardShipment.cs
+++ OOP03/StandardShipment.cs
@@ -5,31 +5,31 @@ using System.Text;
 
 #region standardshipment
 
-namespace OOP03
-{
-    public class StandardShipment : Shipment
-{
-    public StandardShipment(
-        string trackingCode,
-        string description,
-        decimal weight,
-        decimal deliveryFee,
-        DeliveryAddress destination)
-        : base(trackingCode, description, weight, deliveryFee, destination)
-    {
-    }
+//namespace OOP03
+//{
+//    public class StandardShipment : Shipment
+//{
+//    public StandardShipment(
+//        string trackingCode,
+//        string description,
+//        decimal weight,
+//        decimal deliveryFee,
+//        DeliveryAddress destination)
+//        : base(trackingCode, description, weight, deliveryFee, destination)
+//    {
+//    }
 
-    public override void PrintShipment()
-    {
-        Console.WriteLine("Standard Shipment");
-        Console.WriteLine($"Tracking Code : {TrackingCode}");
-        Console.WriteLine($"Description : {Description}");
-        Console.WriteLine($"Weight : {Weight} KG");
-        Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
-        Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
-    }
-}
-}
+//    public override void PrintShipment()
+//    {
+//        Console.WriteLine("Standard Shipment");
+//        Console.WriteLine($"Tracking Code : {TrackingCode}");
+//        Console.WriteLine($"Description : {Description}");
+//        Console.WriteLine($"Weight : {Weight} KG");
+//        Console.WriteLine($"Delivery Fee : {DeliveryFee} EGP");
+//        Console.WriteLine($"Estimated Cost : {EstimatedCost} EGP");
+//    }
+//}
+//}
 
 
 
