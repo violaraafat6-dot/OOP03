afcde20c49f6285974a97dbd9bc2db2f0f5f241d
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-11 03:07:36 +0300

    question2

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -17,13 +17,31 @@ namespace OOP03
             //A derived class provides a specific implementation of a method that already exists in the base class
 
 
-//            b) What is the difference between Static Binding and Dynamic Binding?
+            //            b) What is the difference between Static Binding and Dynamic Binding?
 
-//Static Binding:
-//The method to execute is determined at compile time
+            //Static Binding:
+            //The method to execute is determined at compile time
 
-//Dynamic Binding:
-//The method to execute is determined at runtime depending on the actual object
+            //Dynamic Binding:
+            //The method to execute is determined at runtime depending on the actual object
+
+            #endregion
+
+            #region question2
+
+//            a) What is the purpose of the sealed keyword when applied to a class?
+
+//A sealed class cannot be inherited.It stops the inheritance chain
+
+//b) What is the difference between a sealed class and a sealed method?
+
+//A sealed class prevents other classes from inheriting from it.
+
+//A sealed method prevents further overriding of that method in derived classes
+
+//c) Can a sealed method be overridden? Why?
+//No.A sealed method cannot be overridden because the sealed keyword prevents further overriding down the inheritance chain
+
 
             #endregion
         }
