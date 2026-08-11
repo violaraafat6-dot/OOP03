a061f879cc3ff17fe841b0dabed3bf95f18928fb
violaraafat6-dot <violaraafat6@gmail.com>
2026-08-11 04:12:41 +0300

    driver

--- OOP03/Driver.cs
+++ OOP03/Driver.cs
@@ -4,22 +4,22 @@ using System.Text;
 
 #region driver
 
-namespace OOP03
-{
-    public class Driver
-    {
-        public int DriverId { get; set; }
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
+//namespace OOP03
+//{
+//    public class Driver
+//    {
+//        public int DriverId { get; set; }
+//        public string FullName { get; set; }
+//        public string PhoneNumber { get; set; }
 
-        public Driver(int driverId, string fullName, string phoneNumber)
-        {
-            DriverId = driverId;
-            FullName = fullName;
-            PhoneNumber = phoneNumber;
-        }
-    }
-}
+//        public Driver(int driverId, string fullName, string phoneNumber)
+//        {
+//            DriverId = driverId;
+//            FullName = fullName;
+//            PhoneNumber = phoneNumber;
+//        }
+//    }
+//}
 
 
 #endregion
